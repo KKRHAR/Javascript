d62add1b79cbcee852bc520102719f2a653ad7c8
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-05 10:30:14 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all JS code as newer Version
+ 
+// alert(3+3) // we are using nodejs not browser
+
+let name = "ragen"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
