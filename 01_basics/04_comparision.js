9d9aa5e2ae26bde316174de840957ce4b5dd3550
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-05 15:35:53 +0000

    comparison operator

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null == 0);
+// console.log(null>=0);
+
+// console.log(undefined == 0);
+// console.log(undefined >0);
+// console.log(undefined <0)
+// donot use these type of confusing conversion. 
+// === 
+console.log("2" === 2);
