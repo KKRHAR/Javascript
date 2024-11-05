11bde55944000c4e1ded4b6b318fbe70bf9b5097
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-05 10:51:05 +0000

    Conversion of DataType

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,29 @@
+let score = undefined
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abs" => NaN(Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = "Ragen"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 100 20 1.5 => true; 0 => false
+// "" => false
+// "Ragen" => true
+
+let someNumber = 13
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber)
