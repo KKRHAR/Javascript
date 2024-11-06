dd023e2cbc8f02a74abb293d9d7f348b622c8aa5
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-06 13:03:20 +0000

    Strings in js

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -3,7 +3,7 @@ const repoCount = 50
 
 // console.log(name+ repoCount+ "Value");
 // console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
-
+// this concating style ${} is called string interpolation.
 const gameName = new String("G-Ta-6")
 console.log(gameName.__proto__);
 
