7eb924f69b74833b32c95df6c2a25646629275d7
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-09 09:30:45 +0000

    for of  loops

---
+++ 05_iteration/for.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js:'javascript',
+    cpp:"C++",
+    rb:"ruby",
+    swift:'swift by apple'
+}
+
+// For in loops
+
+// for(const key in myObject){
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const programming=["js","java","py","java","cpp"]
+
+// for (const key in programming){
+//     console.log(programming[key]);//it gives us key value not directly
+//     // the array we can get value by programming[key]
+// }
+
+// const map = new Map()
+
+// map.set('IN',"India")
+// map.set('USA',"USAA")
+
+// for(const key in map){
+//     console.log(key);
+// }
+// // Map is not iterable so we cannot use it it gives us no value
+
