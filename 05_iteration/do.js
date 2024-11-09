7a9568bf640f39ec94748d3e1cac96f552797411
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-09 08:57:09 +0000

    Do while and While loop

---
+++ 05_iteration/do.js
@@ -0,0 +1,34 @@
+
+// while(condition){
+
+// }
+// declaring while loop.
+
+// let index =0
+// while(index<=10){
+// console.log(`value of index is ${index}`);
+//     index = index+2
+// }
+
+let myArray= ["IronMan","Thor","Batman"]
+let arr = 0;
+while(arr<myArray.length){
+    console.log(`My array is ${myArray [arr] }`)
+    arr= arr+1
+}
+
+// do{
+
+// }while(condition)
+
+//Intializing do while loop.
+
+let score= 11
+
+do{
+    console.log(`score is ${score}`);
+    score++
+}while(score<=10)
+
+    // here we can see condition is unsatisifed but still it runs one
+    // ie score is 11.
