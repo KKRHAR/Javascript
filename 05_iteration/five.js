7517aeb51459b4aedbd21327b42906f86c323c81
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-09 09:49:56 +0000

    for each loops

---
+++ 05_iteration/five.js
@@ -0,0 +1,43 @@
+const coding = ["js","ruby","java","python","cpp"];
+
+// array.forEach(element => {
+    
+// });   declaration of for each
+
+// coding.forEach(item=> {
+//     console.log(item)
+// });
+
+// function printme(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printme)// as you can see we can also pass function.
+
+
+// coding.forEach((item,index,array)=>{
+// console.log(item,index,array)
+// })// as we can see we can get item index and whole array from 
+// for each.
+
+
+const myCoding = [
+    {
+        languageName:"javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName:"java",
+        languageFileName:"js"
+    },
+    {
+        languageName:"python",
+        languageFileName:"py",
+    },
+]
+
+
+// myCoding.forEach((item)=>{
+// console.log(item.languageFileName)
+// })
+// here we just accessed object value from array of object.
