e60137eab5c192183260ca2e6e5468ebfb0f2fe2
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-13 07:33:27 +0000

    Number guess

--- 07_projects/Guess a Number/num.js
+++ 07_projects/Guess a Number/num.js
@@ -1,4 +1,4 @@
-const randomnum= parseInt(Math.random()*10+1);
+let randomnum= parseInt(Math.random()*100+1);
 
 const submit = document.querySelector('#subt');
 const userInput= document.querySelector('#guessField');
@@ -18,16 +18,16 @@ if(playGame){
   submit.addEventListener("click",function(e){
     e.preventDefault();
     const guess= parseInt(userInput.value)
-    validateGuess(guess)
+    validateGuess(guess);
   })
 }
 function validateGuess(guess){
-if(!isNaN(guess)){
+if(isNaN(guess)){
   alert("Please enter a valid number")
 }else if(guess<1){
   alert("Please enter number greater than 1")
 }else if(guess>100){
-alert("Plz enter a number greater than 100")
+alert("Plz enter a number less than 100")
 }
 else{
   prevGuess.push(guess)
@@ -42,8 +42,11 @@ else{
 }
 }
 function checkGuess(guess){
-if(guess===randomnum){
+  console.log(`User guess: ${guess}, Random number: ${randomnum}`); // Log for debugging
+
+if(guess === randomnum){
 displayMessage(`You guessed it right`)
+endGame()
 }else if(guess<randomnum){
   displayMessage(`Number is Too low`)
 }else if(guess>randomnum){
@@ -63,8 +66,25 @@ lowOrHi.innerHTML=`<h2>${message}</h2>`
 }
 
 function endGame(){
-
+userInput.value= ''
+userInput.setAttribute('disabled','')
+p.classList.add('button');
+p.innerHTML= '<h2 id="newGame">Start New Game</h2>'
+startOver.appendChild(p)
+playGame= false;
+newGame();
 }
 function newGame(){
+ const newGameButton= document.querySelector('#newGame')
+  newGameButton.addEventListener("click",function(e){
+   randomnum= parseInt(Math.random()*100+1);
+    prevGuess=[];
+    numGuess= 1
+    guessSlot.innerHTML='';
+    remaining.innerHTML=`${11-numGuess}`
+    userInput.removeAttribute('disabled')
+    startOver.removeChild(p)
+    playGame= true;
+  })
 
 }
