1ce51592d61f0c399eb75dca53d79fb8e41a7315
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-21 15:58:39 +0000

    Promise

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -1,13 +1,40 @@
-const promiseOne = new Promise(function(resolve,reject){
-    // Do an async task
-    // DB calls,cryptograph,network
+// const promiseOne = new Promise(function(resolve,reject){
+//     // Do an async task
+//     // DB calls,cryptograph,network
+//     setTimeout(function(){
+//         console.log("Async task is complete");
+//         resolve()
+//     },1000)
+// })
+
+// promiseOne.then(function(){
+//     console.log("Promise consumed");
+// })
+
+const promiseThree = new Promise(function (resolve,reject){
     setTimeout(function(){
-        console.log("Async task is complete");
-        resolve()
+resolve({username:'Ragen',email:"nepal@gmail.com"})
     },1000)
 })
-
-promiseOne.then(function(){
-    console.log("Promise consumed");
+promiseThree.then(function(user){
+console.log(user);
 })
 
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+let error= true
+if(!error){
+    resolve({username:"ABC",password:"123"})
+}else{
+    reject('Error: Something went wrong')
+}
+    },3000)
+})
+
+promiseFour.then((user)=>{console.log(user)
+    console.log(user);
+    return user.username
+});
+
+
+// .catch()
