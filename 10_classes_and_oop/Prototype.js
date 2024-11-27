2203a666e511a427197f34ab140f9e65b126ab00
Ragen Nepal <127957874+KKRHAR@users.noreply.github.com>
2024-11-27 16:16:13 +0000

    Protoype and Inheritance

---
+++ 10_classes_and_oop/Prototype.js
@@ -0,0 +1,61 @@
+// let myName = 'Ragen    '
+
+// console.log(myName.truelength);
+
+// let myHeros= ['thor','spiderman']
+
+// let heroPower = {
+//     thor:"Hammer",
+//     spiderman:"sling",
+//     getSpiderPower:function(){
+//         console.log(`Spidy power is ${this.spiderman}`)
+//     }
+// }
+// Object.prototype.ragen= function(){
+//     console.log(`ragen is present in all object`);
+// }
+// Array.prototype.heyRagen = function(){
+//     console.log(`Hey there`)
+// }
+
+// // heroPower.ragen()
+// // myHeros.ragen()
+// // myHeros.heyRagen()
+// // heroPower.heyRagen()
+
+
+// // Inheritance
+
+// const User = {
+//     name:"ragen",
+//     email:"wqa@gmail.com",
+// }
+// const Teacher = {
+//     isAvailable:false
+// }
+// const TeachingSupport= {
+//     isAvailable:false
+// }
+// const TASupport = {
+//     makeAssignment:'JS assignment',
+//     fullTime:true,
+//     __proto__:TeachingSupport
+// }
+
+
+// Teacher.__proto__= User
+
+// //Modern Synatax
+// Object.setPrototypeOf(TeachingSupport,Teacher)
+
+
+
+// let anotherUsername = "Nepal  "
+// String.prototype.trueLength= function(){
+//     console.log(`${this}`)
+//     console.log(`${this.name}`)
+//     console.log(`True Lenght is :${this.trim().length}`);
+// }
+// anotherUsername.trueLength()
+// "Ragen".trueLength()
+// "Ice".trueLength()
